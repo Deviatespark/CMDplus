a875b300e283a8d38b9ae2113ead062903826226
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-09-25 10:32:00 +0200

    commit:creacion de rama 2.0

--- src/cmdplus/Main.java
+++ src/cmdplus/Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Version 2.0 del programa que produce mejoras en clases*/
 package cmdplus;
 
 import cmdplus.Models.Consola;
@@ -12,7 +8,7 @@ import cmdplus.controller.InterfazPrincipal;
  *
  * @author galve
  */
-public class CMDplus {
+public class Main {
 
     /**
      * @param args the command line arguments
